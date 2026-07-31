4f15f0454a60d2b35b830d97f8e3f3d4efd51767
Batsinda Keynes Benoit <b.benoit@alustudent.com>
2026-07-31 23:10:36 +0200

    Update API_Key.js

--- API_Key.js
+++ API_Key.js
@@ -6,7 +6,7 @@
 const API_CONFIG = {
     baseUrl: 'https://booking-com15.p.rapidapi.com/api/v1/hotels',
     headers: {
-        'x-rapidapi-key': 'df63a52205mshee02062c6a569d7p17be64jsna7f1f6521830',
+        'x-rapidapi-key': '35d35feaf0msh0c6f841f0c55c19p1558ffjsn6942fbe25b5b',
         'x-rapidapi-host': 'booking-com15.p.rapidapi.com'
     },
     cacheExpiry: 24 * 60 * 60 * 1000 // 24 hours in milliseconds
